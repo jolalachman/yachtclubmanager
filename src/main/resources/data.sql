039ece8faef55dd213c5a56121c5564a7b350d66
Jolanta Lachman <jolalac346@student.polsl.pl>
2024-01-04 20:02:22 +0100

    --

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
+DELETE FROM roles;
 INSERT INTO roles (role_name)
 VALUES	  (0),
           (1),
@@ -6,6 +7,7 @@ VALUES	  (0),
           (4),
           (5);
 
+DELETE FROM yacht_statuses;
 INSERT INTO yacht_statuses (yacht_status_name)
 VALUES (0),
        (1),
@@ -14,6 +16,7 @@ VALUES (0),
        (4),
        (5);
 
+DELETE FROM reservation_statuses;
 INSERT INTO reservation_statuses(reservation_status_name)
 VALUES (0),
        (1),
@@ -22,17 +25,20 @@ VALUES (0),
        (4),
        (5);
 
+DELETE FROM sailing_licenses;
 INSERT INTO sailing_licenses(sailing_license_name)
 VALUES (0),
        (1),
        (2),
        (3);
 
+DELETE FROM notice_statuses;
 INSERT INTO notice_statuses(notice_status_name)
 VALUES (0),
        (1),
        (2);
 
+DELETE FROM yacht_types;
 INSERT INTO yacht_types(yacht_type_name)
 VALUES (0),
        (1),
